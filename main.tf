29c30b2e818ccabc67a2d88eb4cf2a9b5ac9bbc0
Lohithkumar <123312695+lohith762002@users.noreply.github.com>
2024-10-23 19:15:24 +0530

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,18 @@
+provider "aws" {
+region = "us-east-1"
+}
+
+resource "aws_instance" "one" {
+count = 4
+ami = "ami-0ddc798b3f1a5117e"
+instance_type = "t2.medium"
+key_name = "bleach"
+vpc_security_groups_ids = ["sg-041e08f4412b38651"]
+tags = {
+Name = var.instance_names[count.index]
+}
+}
+
+variable "instance_names" {
+default = ["jenkins", "nexus", "tomcat-1", "tomcat-2"]
+}
